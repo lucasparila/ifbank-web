bcaa14c6695aa45a89837842852d0816dff0768c
lucasparila <lucas.eparila@gmail.com>
2026-06-13 16:43:45 -0300

    feat: dashboard do gerente, com as funcionalidades de listar contas pendentes de aprovação e ação de aprovar

---
+++ src/app/services/gerente.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContaPendenteDTO, PerfilGerenteCompletoDTO} from '../models/perfil.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GerenteService {
+  private readonly API_URL = 'http://localhost:8080/api/gerente';
+
+  constructor(private http: HttpClient) { }
+
+  obterPerfilCompleto(idUsuario: number): Observable<PerfilGerenteCompletoDTO> {
+    return this.http.get<PerfilGerenteCompletoDTO>(`${this.API_URL}/${idUsuario}`);
+  }
+  obterContasPendentes(): Observable<ContaPendenteDTO[]> {
+    return this.http.get<ContaPendenteDTO[]>(`${this.API_URL}/contas-pendentes`);
+  }
+
+  aprovarConta(idConta: number): Observable<string> {
+    return this.http.put(`${this.API_URL}/aprovar-conta/${idConta}`, {}, { responseType: 'text' });
+  }
+}
